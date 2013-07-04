142d2509ca635d5a02d89c1a2ad36d5674fd283d
Don Park <don@donpark.org>
2013-07-04 13:33:21 -0700

    add jade

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ var app = express();
 r.connect({host:'localhost', port: 28015, db:'activitystream'}, function(err, conn) {
   if(err){ console.log(err) } else {
     console.log('db on')
+    app.set('views', __dirname + '/views')
+    app.set('view engine', 'jade')
+    app.use(express.static(__dirname + '/public'))
     app.get('/',function(req, res){
       console.log(req.route.method+" "+req.originalUrl)
-
-      var body = 'Hello World';
-      res.setHeader('Content-Type', 'text/plain');
-      res.setHeader('Content-Length', body.length);
-      res.end(body);
+      res.render('index');
     });
   }
 })
