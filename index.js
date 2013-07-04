eb2bf2cc8143fd1ac61ce823d12c80ecf058cced
Don Park <don@donpark.org>
2013-07-04 14:26:52 -0700

    dump rows from table 'activity'

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ var express = require('express');
 var app = express();
 
 r.connect({host:'localhost', port: 28015, db:'activitystream'}, function(err, conn) {
+  r.dbCreate('activitystream').run(conn, function(err){})
+  r.tableCreate('activity').run(conn, function(err){if(err){console.log(err)}})
+  //r.table('activity').insert({verb:"checkin", object: {objectType:"place",name:"My Father's Place"}}).run(conn, function(){})
+
   if(err){ console.log(err) } else {
     console.log('db on')
     app.set('views', __dirname + '/views')
     app.set('view engine', 'jade')
     app.use(express.static(__dirname + '/public'))
     app.get('/',function(req, res){
-      console.log(req.route.method+" "+req.originalUrl)
-      res.render('index');
-    });
+      r.table('activity').run(conn, function(err, cursor){
+        console.log(req.route.method+" "+req.originalUrl)
+        cursor.toArray(function(err, rows){
+          res.render('index', {activities:rows});
+        })
+      })
+    })
   }
+
 })
 
 var port = 6106
