009a5d27b4e3c85acf7b604696d5a6e84ac47019
Don Park <donp@donp.org>
2015-08-11 11:23:50 -0700

    put up some gauges

--- public/app.js
+++ public/app.js
@@ -6,3 +6,31 @@ function time_fixup() {
   })
 }
 
+function gauge_go() {
+  var today = new Date()
+  var midnight = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+  var seconds_elapsed = (new Date() - midnight)/1000
+  var day_left = 24 - (seconds_elapsed/60/60)
+  var endurance = (day_left / 24)*100
+  var g1 = new JustGage({
+      id: "gauge1",
+      value: endurance.toFixed(0),
+      min: 0,
+      max: 100,
+      title: "Endurance"
+     });
+  var g2 = new JustGage({
+      id: "gauge2",
+      value: 67,
+      min: 0,
+      max: 100,
+      title: "Food"
+     });
+  var g3 = new JustGage({
+      id: "gauge3",
+      value: 51,
+      min: 0,
+      max: 100,
+      title: "Money"
+     });
+}
